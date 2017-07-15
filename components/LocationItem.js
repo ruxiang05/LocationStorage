dc236a121597590996f289eb7aad845f85904c87
Ruxandra Anghel <ruxiang05@gmail.com>
2017-07-15 19:06:29 +0100

    Add LocationItem.js

---
+++ components/LocationItem.js
@@ -0,0 +1,27 @@
+var React = require('react'),
+    moment = require('moment');
+
+var LocationItem = react.createClass({
+    handleClick() {
+        this.props.onClick(this.props.address);
+    },
+
+    render(){
+        let cn = 'list-group-item';
+
+        if(this.props.active) {
+            cn += 'active-location';
+        }
+
+        return (
+            <a className={cn} onClock={this.handleClick}>
+                {this.props.address}
+                <span className='createdAt'>{moment(this.timestamp).fromNow()}</span>
+                <span className='glyphicon glyphicon-menu-right'></span>
+            </a>
+        )
+    }
+
+});
+
+module.exports = LocationItem;
